1a26b86cc1886d48ec79615020f6c14ebebbab1b
MarianVentura <marianv1119@gmail.com>
2024-11-27 20:31:20 -0400

    Creacion del modelo MetedoPago

---
+++ Models/MetodoPago.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace RentalCar.Models;
+
+public class MetodoPago
+{
+    [Key]
+    public int MetodoPagoId { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    public string? Tipo { get; set; } //Ejemplo: "Tarjeta de Crédito", "Transferencia Bancaria" 
+
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    public DateTime FechaTransaccion { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    public decimal Monto { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    public int ReservaId { get; set; }
+
+    [ForeignKey("ReservaId")]
+    public Reservas? Reserva { get; set; }
+}
