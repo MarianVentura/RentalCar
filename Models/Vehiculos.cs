7ffe578e5cbc0bbd70abe2c4eab34bae75183b86
MarianVentura <marianv1119@gmail.com>
2024-11-27 20:31:41 -0400

    Creacion del modelo Vehiculos

---
+++ Models/Vehiculos.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentalCar.Models;
+
+public class Vehiculos
+{
+    [Key]
+    public int VehiculosId { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    public string? Nombre { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    public string? Marca { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    public string? Modelo { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    public decimal PrecioPorDia { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    public string? Categoria { get; set; }
+    public bool Disponible { get; set; } = true;
+}
