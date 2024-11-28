78e5de68c3781862c22d544c13b41b5f98c23268
MarianVentura <marianv1119@gmail.com>
2024-11-27 20:31:10 -0400

    Creacion del modelo Clientes

---
+++ Models/Clientes.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentalCar.Models;
+
+public class Clientes
+{
+    [Key]
+    public int ClienteId { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio")]
+    public string? Nombres { get; set; }
+
+    [Required(ErrorMessage = "Este campo es oblogatorio.")]
+    [EmailAddress(ErrorMessage = "Correo electrónico no válido.")]
+    public string? Email { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    public string? Telefono { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    public string? Direccion { get; set; }
+    public string? Identificacion { get; set; } // Ejemplo: "Cédula" o "Pasaporte"
+}
+
